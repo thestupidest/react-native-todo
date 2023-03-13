23529384160b717bcbdbf886118937678154554f
Kenny <cywsc1349@hotmail.com>
2023-03-13 16:12:21 +0900

    하이

---
+++ src/images.js
@@ -0,0 +1,13 @@
+import CheckBoxOutline from '../assets/icon/unchecked.png';
+import CheckBox from '../assets/icon/checked.png';
+import Delete from '../assets/icon/delete.png';
+import Edit from '../assets/icon/edit.png';
+
+const images = {
+    uncompleted : CheckBoxOutline,
+    completed : CheckBox,
+    delete : Delete,
+    update : Edit,
+};
+
+export default images;
